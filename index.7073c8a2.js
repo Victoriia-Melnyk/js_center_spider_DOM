acab43949ce821636f12383419c38143e798de1c
Viktoriia Melnyk <melnychka1993@gmail.com>
2024-07-11 16:48:45 +0100

    make build

---
+++ index.7073c8a2.js
@@ -0,0 +1,2 @@
+function e(e){return parseInt(e.replace("px",""),10)}var t=document.querySelector(".wall"),l=document.querySelector(".spider"),o=e(getComputedStyle(t).width),p=e(getComputedStyle(l).width),c=e(getComputedStyle(t).height),r=e(getComputedStyle(l).height);l.style.top="".concat(c/2-r/2,"px"),l.style.left="".concat(o/2-p/2,"px");
+//# sourceMappingURL=index.7073c8a2.js.map
